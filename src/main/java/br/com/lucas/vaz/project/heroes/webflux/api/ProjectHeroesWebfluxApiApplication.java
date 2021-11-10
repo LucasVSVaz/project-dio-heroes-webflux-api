05e1f650447eb77deac219ee03a3c4414fdd2c8e
Lucas Vieira <lucas.v.santosvaz@gmail.com>
2021-11-10 16:38:54 -0300

    Projeto DIO - WebFlux

---
+++ src/main/java/br/com/lucas/vaz/project/heroes/webflux/api/ProjectHeroesWebfluxApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucas.vaz.project.heroes.webflux.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHeroesWebfluxApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectHeroesWebfluxApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/lucas/vaz/project/heroes/webflux/api/ProjectHeroesWebfluxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucas.vaz.project.heroes.webflux.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectHeroesWebfluxApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
